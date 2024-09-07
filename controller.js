c303af54a532b73db8fb0af5c6dfa83b1c8c8a2d
TeguhWahyudiono <queensha.19aam@gmail.com>
2024-09-07 22:47:27 +0700

    step 10 berhasil tambahkan data

--- controller.js
+++ controller.js
@@ -30,3 +30,20 @@ exports.tampilberdasarkanid = function(req, res) {
             }
         });
 };
+
+// menambahkan data mahasiswa
+exports.tambahmahasiswa = function(req, res) {
+    let nama = req.body.nama;
+    let prodi = req.body.prodi;
+    let fakultas = req.body.fakultas;
+
+    connection.query('INSERT INTO mhs (nama, prodi, fakultas) VALUES(?,?,?)',
+        [nama, prodi, fakultas],
+        function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil menambahkan data!", res);
+            }
+        });
+};
