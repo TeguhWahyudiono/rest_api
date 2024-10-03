6f593f8b2b5bc01b52a4939ae8864388fe50e6bf
TeguhWahyudiono <queensha.19aam@gmail.com>
2024-10-04 05:40:13 +0700

    buat verifikasi

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,13 @@
 let express = require('express');
 let auth = require('./auth');
 let router = express.Router();
+let verifikasi = require('./verifikasi');
 
 // daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
+router.post('/api/v1/login', auth.login);
+
+// alamat yang perlu otorisasi
+router.get('/api/v1/rahasia', verifikasi(2), auth.halamanrahasia);
 
 module.exports = router;
